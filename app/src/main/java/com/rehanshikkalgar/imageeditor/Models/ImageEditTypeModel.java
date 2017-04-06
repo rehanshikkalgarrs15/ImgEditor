cddc8a6f96611ddc78e7f5d11319722a8d827986
HoneyWell-Quest <syedmunawwar990@gmail.com>
2017-04-06 22:15:10 +0530

    image Edit Activity Added

---
+++ app/src/main/java/com/rehanshikkalgar/imageeditor/Models/ImageEditTypeModel.java
@@ -0,0 +1,20 @@
+package com.rehanshikkalgar.imageeditor.Models;
+
+/**
+ * Created by rehan r on 06-04-2017.
+ */
+public class ImageEditTypeModel {
+    String editType;
+
+    public ImageEditTypeModel(String editType) {
+        this.editType = editType;
+    }
+
+    public String getEditType() {
+        return editType;
+    }
+
+    public void setEditType(String editType) {
+        this.editType = editType;
+    }
+}
